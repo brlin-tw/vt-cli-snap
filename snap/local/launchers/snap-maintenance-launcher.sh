fd23dc768035d1ae951e2ac857f7aff5792a29b4
林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>
2025-08-03 23:15:43 +0800

    refactor(launcher): Simplify command completion short-curcuit logic
    Signed-off-by: 林博仁(Buo-ren Lin) <buo.ren.lin@gmail.com>

--- snap/local/launchers/snap-maintenance-launcher.sh
+++ snap/local/launchers/snap-maintenance-launcher.sh
@@ -18,39 +18,39 @@ fi
 #
 # $1: /bin/bash
 # $2: /usr/lib/snapd/etelpmoc.sh
-if ! {
-    test "${#}" -ge 2 \
-        && test "${2}" == /usr/lib/snapd/etelpmoc.sh
-    }; then
-    # Ensure that the user has read the notice
-    # NOTE: We can't use snapctl to read the flag because it can only be set by the snap itself when it is run as the superuser.
-    marker_file="${SNAP_USER_COMMON}/.snap.unofficial-notice-shown"
-    if test ! -f "${marker_file}"; then
-        printf \
-            "This is NOT an official distribution of the VirusTotal CLI, refer to the snap's own issue tracker for support:\\n\\n%s\\n\\n" \
-            https://gitlab.com/brlin/vt-cli-snap/-/issues \
-            1>&2
-        printf \
-            'NOTE: This message will only be shown once, the application will be launched in 10 seconds.\n' \
-            1>&2
-        sleep 10
-        touch "${marker_file}"
-        printf '\n--------------------------------\n\n' 1>&2
-    fi
+if test "${#}" -ge 2 \
+    && test "${2}" == /usr/lib/snapd/etelpmoc.sh; then
+    exec "${@}"
+fi
 
-    vt_config_file_native="${SNAP_REAL_HOME}/.vt.toml"
-    vt_config_file_snap="${SNAP_USER_DATA}/.vt.toml"
-    if test -e "${vt_config_file_native}" \
-        && ! test -e "${vt_config_file_snap}"; then
+# Ensure that the user has read the notice
+# NOTE: We can't use snapctl to read the flag because it can only be set by the snap itself when it is run as the superuser.
+marker_file="${SNAP_USER_COMMON}/.snap.unofficial-notice-shown"
+if test ! -f "${marker_file}"; then
+    printf \
+        "This is NOT an official distribution of the VirusTotal CLI, refer to the snap's own issue tracker for support:\\n\\n%s\\n\\n" \
+        https://gitlab.com/brlin/vt-cli-snap/-/issues \
+        1>&2
+    printf \
+        'NOTE: This message will only be shown once, the application will be launched in 10 seconds.\n' \
+        1>&2
+    sleep 10
+    touch "${marker_file}"
+    printf '\n--------------------------------\n\n' 1>&2
+fi
+
+vt_config_file_native="${SNAP_REAL_HOME}/.vt.toml"
+vt_config_file_snap="${SNAP_USER_DATA}/.vt.toml"
+if test -e "${vt_config_file_native}" \
+    && ! test -e "${vt_config_file_snap}"; then
+    printf \
+        "INFO: Migrating the native configuration file to the snap's data directory...\\n" \
+        1>&2
+    if ! cp -a "${vt_config_file_native}" "${vt_config_file_snap}"; then
         printf \
-            "INFO: Migrating the native configuration file to the snap's data directory...\\n" \
+            "Error: Unable to migrate the native configuration file to the snap's data directory.\\n" \
             1>&2
-        if ! cp -a "${vt_config_file_native}" "${vt_config_file_snap}"; then
-            printf \
-                "Error: Unable to migrate the native configuration file to the snap's data directory.\\n" \
-                1>&2
-            exit 2
-        fi
+        exit 2
     fi
 fi
 
